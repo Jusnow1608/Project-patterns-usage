5eaaf3200da61e8f5dc45e0662c80f4eb0021f09
Justyna <justyna.nowak.programista@gmail.com>
2026-01-09 15:13:57 +0100

    setting private variables in class BusinessPlan

--- ProjectPatternsUsage/src/builder/BusinessPlan.java
+++ ProjectPatternsUsage/src/builder/BusinessPlan.java
@@ -5,13 +5,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BusinessPlan {
-    public String persona;
-    public boolean isUnregistered;
-    public boolean isVatActive;
+    private String persona;
+    private boolean isUnregistered;
+    private boolean isVatActive;
 
     public BusinessPlan(String persona, boolean isUnregistered, boolean isVatActive) {
         this.persona = persona;
         this.isUnregistered = isUnregistered;
         this.isVatActive = isVatActive;
+    }
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public boolean isUnregistered() {
+        return isUnregistered;
+    }
+
+    public void setUnregistered(boolean unregistered) {
+        isUnregistered = unregistered;
+    }
+
+    public boolean isVatActive() {
+        return isVatActive;
+    }
+
+    public void setVatActive(boolean vatActive) {
+        isVatActive = vatActive;
     }
 }
--- ProjectPatternsUsage/src/builder/Main.java
+++ ProjectPatternsUsage/src/builder/Main.java
@@ -8,21 +8,21 @@ public class Main {
         //BusinessPlan plan = new BusinessPlan("FREELANCER", true, false);
         BusinessPlan plan = new BusinessPlan("SHOP", false, true);
 
-        System.out.println("PLAN FOR: " + plan.persona);
+        System.out.println("PLAN FOR: " + plan.getPersona());
 
-        if (plan.persona.equals("FREELANCER")) {
+        if (plan.getPersona().equals("FREELANCER")) {
             System.out.println("- [TASK] Create Portfolio");
-        } else if (plan.persona.equals("SHOP")) {
+        } else if (plan.getPersona().equals("SHOP")) {
             System.out.println("- [TASK] Select E-commerce Platform");
         }
 
-        if (plan.isUnregistered) {
+        if (plan.isUnregistered()) {
             System.out.println("- [TASK] Monitor Revenue Limit (Unregistered Activity)");
         } else {
             System.out.println("- [TASK] Register in CEIDG");
         }
 
-        if (plan.isVatActive) {
+        if (plan.isVatActive()) {
             System.out.println("- [TASK] VAT-R Registration");
         }
     }
